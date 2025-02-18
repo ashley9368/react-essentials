d73bde1939628140d6fd15f9140c90897e623777
ashley9368 <ashleygoward@icloud.com>
2025-02-18 14:06:56 +0000

    stateful greeting

--- my-app/src/components/FunctionalGreeting.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// function FunctionalGreeting(){
-//     return<h1>Hello from react!</h1>
-// }
-
-const FunctionalGreeting = () => <h1>Hello from react!!</h1>;
-
-export default FunctionalGreeting;
